1ac534d192c7108b8431d495b784b103d2e5914a
Ejaz Ahmed <43725530+ejaz22@users.noreply.github.com>
2022-10-17 13:03:04 +0530

    Update extract_snipptes.py

--- extract_snipptes.py
+++ extract_snipptes.py
@@ -2,6 +2,29 @@ import re,pysbd,math
 from nltk import RegexpParser, pos_tag
 from nltk.corpus import stopwords
 seg = pysbd.Segmenter(language="en", clean=False)
+#from spacy import nlp
+
+def extract_relation(sent):
+
+  doc = nlp(sent)
+
+  # Matcher class object 
+  matcher = Matcher(nlp.vocab)
+
+  #define the pattern 
+  pattern = [{'DEP':'ROOT'}, 
+            {'DEP':'prep','OP':"?"},
+            {'DEP':'agent','OP':"?"},  
+            {'POS':'ADJ','OP':"?"}] 
+
+  matcher.add("matching_1", None, pattern) 
+
+  matches = matcher(doc)
+  k = len(matches) - 1
+
+  span = doc[matches[k][1]:matches[k][2]] 
+
+  return(span.text)
 
 def extract_snippets(text:str,n:int=2):
         """ 
